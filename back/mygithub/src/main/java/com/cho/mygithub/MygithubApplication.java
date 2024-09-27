c7d78faf2a7676770b79ebdd6a13240df92a4a38
chosunghyun18 <krismon@naver.com>
2024-09-27 21:54:59 +0900

    build : add different types of profile

---
+++ back/mygithub/src/main/java/com/cho/mygithub/MygithubApplication.java
@@ -0,0 +1,13 @@
+package com.cho.mygithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygithubApplication.class, args);
+    }
+
+}
